b3000ce6b6094505c4d2f638e1af908c13412a56
natumn <natsume.pcuse75@gmail.com>
2018-02-07 11:32:45 +0900

    [Add] implementation

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
+import (
+	"go/token"
+	"go/types"
+	"log"
+	"strconv"
+)
+
 func main() {
 	println("result: %v", calc("15*5"))
 }
 
 func calc(formula string) int {
-	return 75
+	answer, err := types.Eval(token.NewFileSet(), types.NewPackage("main", "calc"), token.NoPos, formula)
+	if err != nil {
+		log.Fatal(err)
+	}
+	intAnswer, err := strconv.Atoi(answer.Value.ExactString())
+	if err != nil {
+		log.Fatal(err)
+	}
+	return intAnswer
 }
