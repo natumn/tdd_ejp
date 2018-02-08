fb18e58b226fe72922648fd8db1d8b74c98d4764
natumn <natsume.pcuse75@gmail.com>
2018-02-08 17:41:51 +0900

    [Add] 三角測量

--- calc_test.go
+++ calc_test.go
@@ -5,11 +5,17 @@ import (
 )
 
 func TestCalculator(t *testing.T) {
-	calcStr := "15*5"
-	answer := calc(calcStr)
-	if answer != 75 {
+	calcStrCase1 := "15*5"
+	calcStrCase2 := "6+1"
+	answerCase1 := calc(calcStrCase1)
+	answerCase2 := calc(calcStrCase2)
+
+	if answerCase1 != 75 {
 		t.Fatal("miss calculator.")
 	}
-
-	t.Log(calcStr, "=", answer)
+	t.Log(calcStrCase1, "=", answerCase1)
+	if answerCase2 != 7 {
+		t.Fatal("miss calculator.")
+	}
+	t.Log(calcStrCase2, "=", answerCase2)
 }
